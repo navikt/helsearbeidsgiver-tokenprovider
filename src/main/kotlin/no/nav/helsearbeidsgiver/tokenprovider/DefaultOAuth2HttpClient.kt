01c17f945e9e6afc3027459b35d355fbe6f60442
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-09-27 12:17:00 +0200

    Forsøker med serializering av ekstern klasse

--- src/main/kotlin/no/nav/helsearbeidsgiver/tokenprovider/DefaultOAuth2HttpClient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/tokenprovider/DefaultOAuth2HttpClient.kt
@@ -5,10 +5,15 @@ import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.forms.submitForm
 import io.ktor.http.Parameters
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.json.Json
 import no.nav.security.token.support.client.core.http.OAuth2HttpClient
 import no.nav.security.token.support.client.core.http.OAuth2HttpRequest
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
 import org.slf4j.LoggerFactory
+
+@Serializer(forClass = OAuth2AccessTokenResponse::class)
+object OAuth2AccessTokenResponseSerializer
 
 class DefaultOAuth2HttpClient() : OAuth2HttpClient {
     private val httpClient = createHttpClient()
@@ -16,14 +21,15 @@ class DefaultOAuth2HttpClient() : OAuth2HttpClient {
     override fun post(oAuth2HttpRequest: OAuth2HttpRequest): OAuth2AccessTokenResponse {
         return runBlocking {
             try {
-                return@runBlocking httpClient.submitForm(
+                val data = httpClient.submitForm(
                     url = oAuth2HttpRequest.tokenEndpointUrl.toString(),
                     formParameters = Parameters.build {
                         oAuth2HttpRequest.formParameters.forEach {
                             append(it.key, it.value)
                         }
                     }
-                ).body<OAuth2AccessTokenResponse>()
+                ).body<String>()
+                return@runBlocking Json.decodeFromString(OAuth2AccessTokenResponseSerializer, data)
             } catch (ex: Exception) {
                 if (ex is ClientRequestException) {
                     logger.error(ex.response.body<String>())
